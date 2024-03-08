e5ef728ce3ecc1494b215717e9e1ea1b4f63fea0
Jim Whiteman <jimtron9000@gmail.com>
2024-03-07 17:02:45 -0700

    add notes

--- 4/29.rb
+++ 4/29.rb
@@ -17,14 +17,15 @@ mac = genmac(msg)
 # ATTACKER
 def forge(mac, msg, newtxt)
   1.upto(32) do |kl|
-    p1    = pad("_" * kl + msg).pack("C*")
-    s1    = [mac].pack("H*").unpack("N5")
+    p1    = pad("_" * kl + msg).pack("C*")        #              stub'd-key || msg || original-padding <- what we're interested in
+    s1    = [mac].pack("H*").unpack("N5")         #       state: key        || msg || original-padding
 
-    p2    = pad(p1 + newtxt).pack("C*")
-    h2    = sha1(p2[p1.length..], state: s1, is_padded: true)
-    fmsg  = p1[kl..] + newtxt
+    p2    = pad(p1 + newtxt).pack("C*")           #              stub'd-key || msg || original-padding || newtxt || new-padding <- what we're interested in
+    pld   = p2[p1.length..]                       #                                                       newtxt || new-padding
+    h2    = sha1(pld, state: s1, is_padded: true) # forged-hash: (state ^ see above)                   || newtxt || new-padding
+    fmsg  = p1[kl..] + newtxt                     # forged-msg:                msg || original-padding || newtxt  (...new-padding will be added by the server)
 
-    if valid?(message: fmsg, mac: h2)
+    if valid?(message: fmsg, mac: h2)             # ...does key || forged-msg == forged-hash
       return { status: "forged!", message: fmsg, mac: h2 }
     end
   end
