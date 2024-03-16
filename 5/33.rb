be2dec7c4af58ef7459aaf25bdae535d9515b264
Jim Whiteman <jimtron9000@gmail.com>
2024-03-15 20:08:17 -0600

    Set 5: 33

---
+++ 5/33.rb
@@ -0,0 +1,26 @@
+require "openssl"
+require "pry"
+
+p = "ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024"\
+    "e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd"\
+    "3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec"\
+    "6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f"\
+    "24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361"\
+    "c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552"\
+    "bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff"\
+    "fffffffffffff".to_i(16)
+
+g = 2
+
+a = rand(p)      # alice private
+A = g.pow(a, p)  # alice public
+
+b = rand(p)      # bob private
+B = g.pow(b, p)  # bob public
+
+s1 = B.pow(a, p) # alice calculates S
+s2 = A.pow(b, p) # bob calculates S
+
+k  = OpenSSL::Digest::MD5.hexdigest(s1.to_s)
+puts "a:\t#{a}\nb:\t#{b}\ns1:\t#{s1}\ns2:\t#{s2}\nk:\t#{k}"
+puts s1 == s2
